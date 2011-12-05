a62fb700dcb4708a77b1534055422512461bb4f1
vzurczak <vincent.zurczak@petalslink.com>
2011-12-05 17:09:20 +0100

    Merge

--- plugins/com.ebmwebsourcing.petals.services/src/com/ebmwebsourcing/petals/services/Messages.java
+++ plugins/com.ebmwebsourcing.petals.services/src/com/ebmwebsourcing/petals/services/Messages.java
@@ -6,6 +6,26 @@ public class Messages extends NLS {
 
 	public static String services;
 	public static String notValidQName;
+	public static String consumeDescription;
+	public static String provideDescription;
+	public static String confimeRemoveEndpointMessage;
+	public static String confimeRemoveEndpointTitle;
+	public static String version;
+	public static String addService;
+	public static String serviceName;
+	public static String serviceMode;
+	public static String selectComponent;
+	public static String createProvidesSU;
+	public static String createProvidesSUDescription;
+	public static String createConsumesSU;
+	public static String createConsumesSUDescription;
+	public static String edit;
+	public static String interfaceQName;
+	public static String serviceQName;
+	public static String namespace;
+	public static String localPart;
+	public static String provides;
+	public static String consumes;
 
 	static {
 		initializeMessages("messages", Messages.class);
