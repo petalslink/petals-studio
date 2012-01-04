669aedffa917484976569ea1d1580ecfdfe40bf2
Mickael Istria <mickael.istria@petalslink.com>
2012-01-04 14:13:17 +0100

    * Added Drag'n'Drop of Petals Services to Eclipse BPEL editor (depends on Eclipse bug 367841)

--- plugins/com.ebmwebsourcing.petals.services.bpel/src-designer/com/ebmwebsourcing/petals/services/bpel/designer/Startup.java
+++ plugins/com.ebmwebsourcing.petals.services.bpel/src-designer/com/ebmwebsourcing/petals/services/bpel/designer/Startup.java
@@ -12,8 +12,17 @@
 
 package com.ebmwebsourcing.petals.services.bpel.designer;
 
+import org.eclipse.bpel.ui.BPELEditor;
+import org.eclipse.bpel.ui.BPELMultipageEditorPart;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IPartListener2;
 import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbenchPartReference;
+import org.eclipse.ui.PlatformUI;
 
+import com.ebmwebsourcing.petals.services.bpel.designer.dnd.PetalsDropTargetListener;
 import com.ebmwebsourcing.petals.services.bpel.designer.provisional.PetalsBpelModules;
 
 /**
@@ -29,5 +38,65 @@ public class Startup implements IStartup {
 		// Initialize the Petals BPEL modules
 		PetalsBpelModules.getBpelEngineValidator();
 		PetalsBpelModules.getBpelEngineAnalyzer();
+		// Listener for DND of PetalsServices to BPEL Designer
+		// Alternative implementation. See Eclipse bug 367715 for another approach
+		Display.getDefault().syncExec(new Runnable() {
+			@Override
+			public void run() {
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().addPartListener(new IPartListener2() {
+					@Override
+					public void partVisible(IWorkbenchPartReference partRef) {
+					}
+					
+					@Override
+					public void partOpened(IWorkbenchPartReference partRef) {
+						if (partRef instanceof IEditorReference) {
+							IEditorReference editorRef = (IEditorReference)partRef;
+							if (editorRef.getEditor(false) instanceof BPELEditor) {
+								BPELEditor bpelEd = (BPELEditor)editorRef.getEditor(false);
+								bpelEd.getGraphicalViewer().addDropTargetListener(new PetalsDropTargetListener(bpelEd.getGraphicalViewer(), bpelEd));
+							} else if (editorRef.getEditor(false) instanceof BPELMultipageEditorPart) {
+								BPELMultipageEditorPart bpelEd = (BPELMultipageEditorPart) editorRef.getEditor(false);
+								IEditorPart ed = bpelEd.getActiveEditor();
+								if (ed instanceof BPELEditor) {
+									BPELEditor editor = (BPELEditor) ed;
+									((BPELEditor)ed).getGraphicalViewer().addDropTargetListener(new PetalsDropTargetListener(editor.getGraphicalViewer(), editor));	
+								}
+								
+							}
+						}
+						
+					}
+					
+					@Override
+					public void partInputChanged(IWorkbenchPartReference partRef) {
+					}
+					
+					@Override
+					public void partHidden(IWorkbenchPartReference partRef) {
+					}
+					
+					@Override
+					public void partDeactivated(IWorkbenchPartReference partRef) {
+					}
+					
+					@Override
+					public void partClosed(IWorkbenchPartReference partRef) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void partBroughtToTop(IWorkbenchPartReference partRef) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void partActivated(IWorkbenchPartReference partRef) {
+					}
+				});
+			}
+		});
 	}
 }
