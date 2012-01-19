4c1cdd42828bcc1ca2e402a359ca1b3cd5b22edd
Mickael Istria <mickael.istria@petalslink.com>
2012-01-19 12:41:00 +0100

    * SFTP Editor Contribution * SQL Editor Contribution * SOAP Editor Contribution

---
+++ plugins/com.ebmwebsourcing.petals.services.sql/src/com/ebmwebsourcing/petals/services/sql/editor/SqlEditorContribution.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+ * Copyright (c) 2012, EBM WebSourcing
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     EBM WebSourcing - initial API and implementation
+ *******************************************************************************/
+package com.ebmwebsourcing.petals.services.sql.editor;
+
+import com.ebmwebsourcing.petals.services.su.editor.extensibility.EditorContributionSupport;
+import com.ebmwebsourcing.petals.services.su.editor.extensibility.JbiEditorDetailsContribution;
+
+/**
+ * 
+ * @author Mickael Istria (EBM Websourcing)
+ *
+ */
+public class SqlEditorContribution extends EditorContributionSupport {
+
+	@Override
+	protected JbiEditorDetailsContribution getProvidesContribution() {
+		return new SqlProvidesEditorContribution();
+	}
+
+	@Override
+	protected JbiEditorDetailsContribution getConsumesContribution() {
+		return null;
+	}
+
+}
---
+++ plugins/com.ebmwebsourcing.petals.services.sql/src/com/ebmwebsourcing/petals/services/sql/editor/SqlProvidesEditorContribution.java
@@ -0,0 +1,77 @@
+/****************************************************************************
+ * 
+ * Copyright (c) 2012, EBM WebSourcing
+ * 
+ * This source code is available under agreement available at
+ * http://www.petalslink.com/legal/licenses/petals-studio
+ * 
+ * You should have received a copy of the agreement along with this program.
+ * If not, write to EBM WebSourcing (4, rue Amelie - 31200 Toulouse, France).
+ * 
+ *****************************************************************************/
+package com.ebmwebsourcing.petals.services.sql.editor;
+
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Section;
+
+import com.ebmwebsourcing.petals.common.internal.provisional.formeditor.ISharedEdition;
+import com.ebmwebsourcing.petals.jbi.editor.form.cdk5.model.cdk5.Cdk5Package;
+import com.ebmwebsourcing.petals.services.cdk.editor.CDK5JBIEndpointUIHelper;
+import com.ebmwebsourcing.petals.services.sql.sql.SqlPackage;
+import com.ebmwebsourcing.petals.services.su.editor.extensibility.JbiEditorDetailsContribution;
+import com.ebmwebsourcing.petals.services.su.editor.su.JBIEndpointUIHelpers;
+import com.sun.java.xml.ns.jbi.AbstractEndpoint;
+
+/**
+ * @author Mickael Istria (EBM Websourcing)
+ *
+ */
+public class SqlProvidesEditorContribution implements JbiEditorDetailsContribution {
+
+	public void addMainSUContent(final AbstractEndpoint endpoint, FormToolkit toolkit, final Composite mainTab, ISharedEdition ise) {
+		mainTab.setLayout(new GridLayout(1, false));
+		mainTab.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		Section identificationSection = toolkit.createSection(mainTab, Section.EXPANDED | Section.TITLE_BAR);
+		identificationSection.setText("Identification");
+		identificationSection.setLayoutData(new GridData(GridData.FILL_BOTH));
+		Composite identificationComposite = toolkit.createComposite(identificationSection);
+		identificationComposite.setLayout(new GridLayout(2, false));
+		identificationSection.setClient(identificationComposite);
+
+		CDK5JBIEndpointUIHelper.createProvidesUI(endpoint, toolkit, identificationComposite, ise);
+		JBIEndpointUIHelpers.createCommonEndpointUI(endpoint, toolkit, identificationComposite, ise);
+
+	}
+
+	public void addAdvancedSUContent(AbstractEndpoint endpoint, FormToolkit toolkit, Composite advancedTab, ISharedEdition ise) {
+		advancedTab.setLayout(new GridLayout(1, false));
+		advancedTab.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		{
+			Section ejbSection = toolkit.createSection(advancedTab, Section.EXPANDED | Section.TITLE_BAR);
+			ejbSection.setText("SQL");
+			ejbSection.setLayoutData(new GridData(GridData.FILL_BOTH));
+			Composite ejbComposite = toolkit.createComposite(ejbSection);
+			ejbComposite.setLayout(new GridLayout(2, false));
+			ejbSection.setClient(ejbComposite);
+			
+			JBIEndpointUIHelpers.createDefaultWidgetsByEIntrospection(endpoint, toolkit, ejbComposite, ise, SqlPackage.Literals.SQL_PROVIDES);
+		}
+		{
+			Section cdkSection = toolkit.createSection(advancedTab, Section.EXPANDED | Section.TITLE_BAR);
+			cdkSection.setText("CDK");
+			cdkSection.setLayoutData(new GridData(GridData.FILL_BOTH));
+			Composite cdkComposite = toolkit.createComposite(cdkSection);
+			cdkComposite.setLayout(new GridLayout(2, false));
+			cdkSection.setClient(cdkComposite);
+			
+			JBIEndpointUIHelpers.createDefaultWidgetsByEIntrospection(endpoint, toolkit, cdkComposite, ise, Cdk5Package.Literals.CDK5_PROVIDES);
+		}
+	}
+
+
+}
