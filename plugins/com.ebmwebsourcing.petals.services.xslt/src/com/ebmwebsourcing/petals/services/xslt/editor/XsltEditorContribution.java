1df9b407f25c8616298afdc4a6d291c8d761e684
Mickael Istria <mickael.istria@petalslink.com>
2012-01-19 13:02:14 +0100

    * XSLT Editor Contribution * XSDL Editor Contribution

---
+++ plugins/com.ebmwebsourcing.petals.services.xslt/src/com/ebmwebsourcing/petals/services/xslt/editor/XsltEditorContribution.java
@@ -0,0 +1,39 @@
+/****************************************************************************
+ * 
+ * Copyright (c) 2012, EBM WebSourcing
+ * 
+ * This source code is available under agreement available at
+ * http://www.petalslink.com/legal/licenses/petals-studio
+ * 
+ * You should have received a copy of the agreement along with this program.
+ * If not, write to EBM WebSourcing (4, rue Amelie - 31200 Toulouse, France).
+ * 
+ *****************************************************************************/
+package com.ebmwebsourcing.petals.services.xslt.editor;
+
+import com.ebmwebsourcing.petals.services.su.editor.extensibility.EditorContributionSupport;
+import com.ebmwebsourcing.petals.services.su.editor.extensibility.JbiEditorDetailsContribution;
+
+/**
+ * @author Mickael Istria (EBM Websourcing)
+ *
+ */
+public class XsltEditorContribution extends EditorContributionSupport {
+
+	/* (non-Javadoc)
+	 * @see com.ebmwebsourcing.petals.services.su.editor.extensibility.EditorContributionSupport#getProvidesContribution()
+	 */
+	@Override
+	protected JbiEditorDetailsContribution getProvidesContribution() {
+		return new XsltProvidesEditorContribution();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ebmwebsourcing.petals.services.su.editor.extensibility.EditorContributionSupport#getConsumesContribution()
+	 */
+	@Override
+	protected JbiEditorDetailsContribution getConsumesContribution() {
+		return null;
+	}
+
+}
